f1a63e152d75300ed1ae930350931d9fc2a5faad
kimhanjune93@gmail.com <kimhanjune93@gmail.com>
2021-08-28 22:37:10 +0900

    p.341 to p. 351

--- sp5-chap12/src/main/java/config/MvcConfig.java
+++ sp5-chap12/src/main/java/config/MvcConfig.java
@@ -4,11 +4,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import controller.RegisterRequestValidator;
 
 @Configuration
 @EnableWebMvc
@@ -38,4 +41,9 @@ public class MvcConfig implements WebMvcConfigurer{
 		ms.setDefaultEncoding("UTF-8");
 		return ms;
 	}
+	
+//	@Override
+//	public Validator getValidator() {
+////		return new RegisterRequestValidator();
+//	}
 }
--- sp5-chap12/src/main/java/controller/RegisterController.java
+++ sp5-chap12/src/main/java/controller/RegisterController.java
@@ -1,8 +1,12 @@
 package controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,8 +41,8 @@ public class RegisterController {
 	}
 	
 	@PostMapping("register/step3")
-	public String handleStep3(RegisterRequest regReq, Errors errors) {
-		new RegisterRequestValidator().validate(regReq, errors);
+	public String handleStep3(@Valid RegisterRequest regReq, Errors errors) {
+		//new RegisterRequestValidator().validate(regReq, errors);
 		if(errors.hasErrors())
 			return "register/step2";
 		try {
@@ -50,5 +54,10 @@ public class RegisterController {
 			return "register/step2";
 		}
 	}
+	
+//	@InitBinder
+//	protected void initBinder(WebDataBinder binder) {
+//		binder.setValidator(new RegisterRequestValidator());
+//	}
 
 }
