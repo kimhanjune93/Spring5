f5bd02fe756a98085e0b1b5b3392c6dc143cf4bb
kimhanjune93@gmail.com <kimhanjune93@gmail.com>
2021-09-01 21:54:36 +0900

    p.353 to p.381

---
+++ sp5-chap13/src/main/java/spring/ChangePasswordService.java
@@ -0,0 +1,26 @@
+package spring;
+
+import java.sql.SQLException;
+
+import org.springframework.transaction.annotation.Transactional;
+
+public class ChangePasswordService {
+
+	private MemberDao memberDao;
+	
+	@Transactional(rollbackFor = SQLException.class)
+	public void changePassword(String email, String oldPwd, String newPwd) {
+		Member member = memberDao.selectByEmail(email);
+		if (member == null)
+			throw new MemberNotFoundException();
+
+		member.changePassword(oldPwd, newPwd);
+
+		memberDao.update(member);
+	}
+
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+}
